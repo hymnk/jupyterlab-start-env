943b9cf17a82a34ef94c56206ddc6e7d01b2ccf3
hymnk3 <ymnk8810@gmail.com>
2021-08-17 11:14:26 +0900

    rename README.rst -> README.md

--- run_jupyterlab.sh
+++ run_jupyterlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+poetry shell
 jupyter lab --port=8890 --allow-root --NotebookApp.token='' --no-browser >> jupyter.log 2>&1 &
 
 #jupyter lab --port=8890 --ip=0.0.0.0 --allow-root --NotebookApp.token='' --no-browser
